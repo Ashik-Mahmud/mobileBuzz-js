19c372a7466ee95c85c141c058c146eec0348117
ashikmahmud <ashikmamud187@gmail.com>
2022-03-01 01:33:20 +0600

    cool! i got a all data from api by search terms

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 /* 
 TODO:
--
+- 
 -
 -
 -
@@ -9,3 +9,21 @@ TODO:
 -
 
 */
+
+/* 1. select all important elements  */
+const searchField = document.getElementById("search-field");
+const searchButton = document.getElementById("search-btn");
+
+
+/* 2. load all mobile phone when users click on search button  */
+searchButton.addEventListener('click', async (event)=>{
+    let searchTerms = searchField.value.toLowerCase();
+    let response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchTerms}`);
+    let data = await response.json();
+    displayPhone(data)
+})
+
+/* 3. display phone item at UI  */
+const displayPhone = (phones) =>{
+    console.log(phones)
+}
